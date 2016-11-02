726a4908bf9edfaf5300db0409eb7b217c106be2
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-11-01 22:59:55 -0400

    Use scalaz-streams. Abandon twitter4j client.

--- build.sbt
+++ build.sbt
@@ -5,25 +5,20 @@ val http4sVersion = "0.14.10"
 val circeVersion = "0.5.4"
 
 libraryDependencies ++= Seq(
-  "org.http4s"    %% "http4s-dsl"          % http4sVersion,
-  "org.http4s"    %% "http4s-blaze-server" % http4sVersion,
-  "org.http4s"    %% "http4s-blaze-client" % http4sVersion,
-  "org.http4s"    %% "http4s-circe"        % http4sVersion,
-  "io.circe"      %% "circe-generic"       % circeVersion,
-  "org.twitter4j" % "twitter4j-stream"     % "4.0.5",
-  "org.twitter4j" % "twitter4j-core"       % "4.0.5"
+  "org.http4s" %% "http4s-blaze-client" % "0.14.11a",
+  "org.http4s" %% "http4s-circe"        % "0.14.11a"
 )
 
 // credit: https://tpolecat.github.io/2014/04/11/scalac-flags.html
 scalacOptions ++= Seq(
-  "-deprecation",
+  //"-deprecation",
   "-encoding", "UTF-8",       // yes, this is 2 args
   "-feature",
   "-language:existentials",
   "-language:higherKinds",
   "-language:implicitConversions",
   "-unchecked",
-  "-Xfatal-warnings",
+  //"-Xfatal-warnings",
   "-Xlint",
   "-Yno-adapted-args",
   "-Ywarn-dead-code",        // N.B. doesn't work well with the ??? hole
--- src/main/scala/net/Main.scala
+++ src/main/scala/net/Main.scala
@@ -1,20 +1,46 @@
 package net
 
-import org.http4s.server.{Server, ServerApp}
-import org.http4s.server.blaze.BlazeBuilder
+import org.http4s._
+import org.http4s.client.blaze._
+import org.http4s.client.oauth1
 import scalaz.concurrent.Task
-import net.service.TweetService
-import net.web.TweetHttpService
+import scalaz.stream.Process
+import jawnstreamz._
+import io.circe.Json
+import java.time.{LocalDateTime, Duration}
 
-object Main extends ServerApp {
+object Main {
 
-  override def server(args: List[String]): Task[Server] = for {
-  	_      <- Task { TweetService.sample }
-  	server <- (
-		BlazeBuilder
-      		.bindHttp(8080, "localhost")
-      		.mountService(TweetHttpService.summary, "/api")
-      		.start
-  		)
-  } yield server
+	private val AccessToken       = "23021955-NeZKh2JAYW1Q5y0Ki9RQYtBtydNxN5ObHm7lNdIw2"
+	private val AccessTokenSecret = "o6SZfroTRuR5VmDqYwYvH8N0ea2sD3DNn0TcgYqJuJolZ"
+
+	private val TwitterStreamingSample = "https://stream.twitter.com/1.1/statuses/sample.json"
+
+	private val ConsumerKey    = "AOvkb6d11mvmB7rI6zymrCQYZ"
+	private val ConsumerSecret = "sLTDy0GR4Htqku0keM7znXgjXnMED9w1P1S3gbxLBkhk3JzILN"
+
+	// Credit to Paul Snively for the following code.
+	// http://scastie.org/23401
+
+	implicit val f = io.circe.jawn.CirceSupportParser.facade
+	val consumer = oauth1.Consumer(ConsumerKey, ConsumerSecret)
+
+	val client = PooledHttp1Client()
+
+	def stati(tokenAccess: String, tokenSecret: String)(uri: String): Process[Task, Json] = {
+		val token = oauth1.Token(tokenAccess, tokenSecret)
+		for {
+			uri  <- Process.eval(Task.fromDisjunction(Uri.fromString(uri)))
+			twapi  = Request(uri = uri)
+			signed = oauth1.signRequest(twapi, consumer, callback = None, verifier = None, token = Some(token))
+			req  <- Process.eval(signed)
+			res  <- client.streaming(req)(resp => resp.body.parseJsonStream)
+		} yield res
+	}
+
+	def main(args: Array[String]): Unit = {
+	   val count = stati(AccessToken, AccessTokenSecret)(TwitterStreamingSample).take(10).runLog.run
+	   println(count)
+	}
+
 }
