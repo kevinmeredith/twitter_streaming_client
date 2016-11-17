8c6a7be411da996d973316dc8744391be1ee6d0a
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-11-16 21:43:45 -0500

    Add EmojiService tests.

--- src/test/scala/net/model/EmojiServiceSpec.scala
+++ src/test/scala/net/model/EmojiServiceSpec.scala
@@ -7,22 +7,64 @@ import org.scalatest.FlatSpec
 
 class EmojiServiceSpec extends FlatSpec {
 
-//  "Reading the emoji-shortened.json file" should "produce a valid List of [[net.model.Emoji]]'s." in {
-//    val url      = this.getClass().getResource("/emoji-shortened.json")
-//    val file     = new File(url.toURI)
-//    val result   = EmojiService.read(file).unsafePerformSync
-//    val expected1 = Emoji(Some( "COPYRIGHT SIGN" ),  "00A9" )
-//    val expected2 = Emoji(Some( "REGISTERED SIGN" ), "00AE" )
-//    assert(result.size == 2)
-//    assert(result.contains(expected1))
-//    assert(result.contains(expected2))
-//  }
-//
-//  "Reading the emoji.json, i.e. the full Emoji file" should "decode successfully" in {
-//    val url      = this.getClass().getResource("/emoji.json")
-//    val file     = new File(url.toURI)
-//    val result   = EmojiService.read(file).unsafePerformSync
-//    assert(result.nonEmpty)
-//  }
+  "Reading the emoji-shortened.json file" should "produce a valid List of [[net.model.Emoji]]'s." in {
+    val url      = this.getClass().getResource("/emoji-shortened.json")
+    val file     = new File(url.toURI)
+    val result   = EmojiService.read(file).unsafePerformSync
+    val expected1 = Emoji.fromString(Some( "COPYRIGHT SIGN" ),  "00A9" ).get
+    val expected2 = Emoji.fromString(Some( "REGISTERED SIGN" ), "00AE" ).get
+    assert(result.size == 2)
+    assert(result.contains(expected1))
+    assert(result.contains(expected2))
+  }
 
+  "Reading the emoji.json, i.e. the full Emoji file" should "decode successfully" in {
+    val url      = this.getClass().getResource("/emoji.json")
+    val file     = new File(url.toURI)
+    val result   = EmojiService.read(file).unsafePerformSync
+    assert(result.nonEmpty)
+  }
+
+  "Checking for the existence of an Emoji" should "succeed if a single unicode emoji present" in {
+    val exclamation = Emoji.fromString(Some( "exclamation" ),  "0021" ).get
+    val input       = "foo bar bippy - ends with exclamation point \u0021"
+    assert( EmojiService.exists(exclamation, input) == Some(exclamation) )
+  }
+
+  import EmojiSpec.readHexes
+
+  "Checking for the existence of an Emoji" should "succeed if a size 2 unicode emoji present" in {
+    val fake    = Emoji.fromString(Some( "fake" ),  "0021-0022" ).get
+    val unicode = readHexes("0021", "0022")
+    val input   = s"$unicode  blah blah blah lasdfasdf."
+    assert( EmojiService.exists(fake, input) == Some(fake) )
+  }
+
+  "Checking for the existence of an Emoji" should "fail if there's no emoji present in the input String" in {
+    val absent = Emoji.fromString(Some( "fake" ),  "0021-0022" ).get
+    val input = "no unicode to see here..... blah blah blah"
+    assert( EmojiService.exists(absent, input).isEmpty )
+  }
+
+  "Finding all emojis in an input String" should "find all input emojis" in {
+    val exclamation    = Emoji.fromString(Some( "exclamation" ),  "0021" ).get
+    val fake           = Emoji.fromString(Some( "fake" ),  "0021-0022" ).get
+    val fakeUnicodeStr = readHexes("0021", "0022")
+    val input = s"hello \u0021 world $fakeUnicodeStr"
+    assert(EmojiService.findAll(List(exclamation, fake), input) == List(exclamation, fake))
+  }
+
+  "Finding all emojis in an input String" should "find all but 1 input emojis" in {
+    val exclamation    = Emoji.fromString(Some( "exclamation" ),  "0021" ).get
+    val fake           = Emoji.fromString(Some( "fake" ),  "0021-0022" ).get
+    val input = s"hello \u0021 world how's it going"
+    assert(EmojiService.findAll(List(exclamation, fake), input) == List(exclamation))
+  }
+
+  "Finding all emojis in an input String" should "find only the input emoji" in {
+    val fake           = Emoji.fromString(Some( "fake" ),  "0021-0022" ).get
+    val fakeUnicodeStr = readHexes("0021", "0022")
+    val input = s"hello \u0021 world $fakeUnicodeStr"
+    assert(EmojiService.findAll(List(fake), input) == List(fake))
+  }
 }
--- src/test/scala/net/model/EmojiSpec.scala
+++ src/test/scala/net/model/EmojiSpec.scala
@@ -2,16 +2,20 @@ package net.model
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class EmojiSpec extends FlatSpec with Matchers {
-  
-  val noName = None
-  val name = Some("some emoji")
-
+object EmojiSpec {
   def readHex(hex: String): String =
     Integer.parseInt(hex, 16).toChar.toString
 
   def readHexes(hex1: String, hex2: String): String =
     readHex(hex1) + readHex(hex2)
+}
+
+class EmojiSpec extends FlatSpec with Matchers {
+
+  import EmojiSpec._
+
+  val noName = None
+  val name = Some("some emoji")
 
   "Constructing an Emoji" should "succeed if the input is a single unicode of 4-5 characters in length and HEX" in {
     assert( Emoji.fromString(noName, "0021").map(_.unicode)  == Some(readHex("0021")))
@@ -22,10 +26,22 @@ class EmojiSpec extends FlatSpec with Matchers {
 
   "Constructing an Emoji" should "succeed if the input consists of two unicode's of 4-5 characters in length and HEX" in {
     assert( Emoji.fromString(noName, "0021-0022").map(_.unicode)  == Some(readHexes("0021", "0022")))
-//    assert( Emoji.fromString(noName, "0023A").map(_.unicode) == Some(readHex("0023A")))
-//    assert( Emoji.fromString(name,   "AAAA").map(_.unicode)  == Some(readHex("AAAA")))
-//    assert( Emoji.fromString(name,   "BBBBB").map(_.unicode) == Some(readHex("BBBBB")))
+    assert( Emoji.fromString(noName, "abcde-abcdf").map(_.unicode)  == Some(readHexes("abcde", "abcdf")))
+    assert( Emoji.fromString(noName, "00001-2222").map(_.unicode)  == Some(readHexes("00001", "2222")))
+  }
+
+  "Constructing an Emoji" should "fail if the input is HEX, but not 4-5 characters long." in {
+    assert( Emoji.fromString(noName, "42").isEmpty )
+    assert( Emoji.fromString(noName, "AAA").isEmpty )
+    assert( Emoji.fromString(name, "AAABBBCCCDDD").isEmpty )
+  }
+
+  "Constructing an Emoji" should "fail if the input is not HEX." in {
+    assert( Emoji.fromString(noName, "abcX").isEmpty )
+    assert( Emoji.fromString(noName, "FIFO").isEmpty )
+    assert( Emoji.fromString(name, "aaaZ").isEmpty )
   }
 
 }
+
 
